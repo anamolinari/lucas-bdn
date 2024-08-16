0ebcbf46b9d8b0b5a686b7c8bf53b2e0f431ab65
Final Project <anahmolinari@gmail.com>
2024-08-17 01:06:24 +0200

    border-color update

--- app/components/Divider.tsx
+++ app/components/Divider.tsx
@@ -4,10 +4,7 @@ interface DividerProps {
 
 export function Divider({ title }: DividerProps) {
   return (
-    <div
-      className="pt-6 pb-3 my-10 border-b border-black/10"
-      aria-label={title}
-    >
+    <div className="pt-6 pb-3 my-10 border-b" aria-label={title}>
       <h2 className="text-sm leading-5">{title}</h2>
     </div>
   );
