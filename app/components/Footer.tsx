b5a2cd0ac8852758b245f5c5d647664ded94d8b2
Final Project <anahmolinari@gmail.com>
2024-08-18 20:21:22 +0200

    Update margin bottom of Footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer
-      className="flex items-center justify-between pt-16 pb-6 border-t-[0.75px] border-line"
+      className="flex items-center justify-between pt-16 pb-6 border-t-[0.75px] border-line sm:mb-20"
       aria-label="Website footer"
     >
       <small className="text-xs leading-4 text-secondary font-semibold tracking-wide opacity-50">
