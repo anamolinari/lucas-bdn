170a433c9c6fb7c4af277a17fe06fe7c1263eb1a
Final Project <anahmolinari@gmail.com>
2024-08-18 20:16:48 +0200

    Update space-y PageContent

--- app/components/PageContent.tsx
+++ app/components/PageContent.tsx
@@ -1,15 +1,15 @@
 interface ContentSectionProps {
-    title: string;
-    children: React.ReactNode;
+  title: string;
+  children: React.ReactNode;
 }
 
 export function PageContent({ title, children }: ContentSectionProps) {
-    return (
-        <div className="flex flex-col text-start">
-            <h3 className="text-base leading-6">{title}</h3>
-            <div className="text-sm leading-5 text-secondary space-y-3">
-                {children}
-            </div>
-        </div>
-    )
+  return (
+    <div className="flex flex-col text-start space-y-3">
+      <h3 className="text-base leading-6">{title}</h3>
+      <div className="text-sm leading-5 text-secondary space-y-3">
+        {children}
+      </div>
+    </div>
+  );
 }
