c8d564665706d6b57869d0c3f3120480530923b0
lucasbordignon0 <l.bordignon0@gmail.com>
2024-08-24 18:38:57 +0200

    updates + new page

--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -6,6 +6,7 @@ import CardsTest from "./playground-components/animated-card/CardsTest";
 import FileDrop from "./playground-components/file-drop/FileDrop";
 import ItemDescription from "./playground-components/item-description";
 import NavBarTest from "./playground-components/nav-bar-playground/NavBarTest";
+import AudioSlider from "./playground-components/random-ones/AudioSlider";
 import LocationCard from "./playground-components/random-ones/LocationCard";
 
 export default function Playground() {
@@ -26,21 +27,6 @@ export default function Playground() {
 
       <FadeInWhenVisible delay={0.2}>
         <section className="flex flex-col gap-6 mt-4">
-          <div className="w-full aspect-video rounded-xl border border-[#1D1F1E]/10 bg-black overflow-hidden">
-            <span className="flex w-full h-full items-center justify-center">
-              <NavBarTest />
-            </span>
-          </div>
-          <ItemDescription
-            title="Floating Navbar"
-            description="First iteration of the floating navbar to figure out the logic behind it, using functions to keep only one active at a time."
-            badges={["Next.js", "Tailwind CSS"]}
-          />
-        </section>
-      </FadeInWhenVisible>
-
-      <FadeInWhenVisible delay={0.2}>
-        <section className="flex flex-col gap-6 mt-4">
           <div className="w-full aspect-video rounded-xl border border-[#1D1F1E]/10 bg-neutral-100 hover:bg-neutral-200/30 transition-colors duration-500 overflow-hidden">
             <span className="flex w-full h-full items-center justify-center">
               <FileDrop />
@@ -56,27 +42,6 @@ export default function Playground() {
 
       <FadeInWhenVisible delay={0.2}>
         <section className="flex flex-col gap-6 mt-4">
-          <div className="w-full aspect-video rounded-xl border border-[#1D1F1E]/10 bg-stone-200 overflow-hidden">
-            <span className="flex w-full h-full items-center justify-center gap-4">
-              <p className="text-sm text-primary font-mono font-semibold">
-                Join Our
-              </p>
-              <CardsTest />
-              <p className="text-sm text-primary font-mono font-semibold">
-                Community
-              </p>
-            </span>
-          </div>
-          <ItemDescription
-            title="Animated Images"
-            description="Inline animation with Framer Motion. Bringing life to regular headings with snappy transitions."
-            badges={["Next.js", "Tailwind CSS", "Framer Motion"]}
-          />
-        </section>
-      </FadeInWhenVisible>
-
-      <FadeInWhenVisible delay={0.2}>
-        <section className="flex flex-col gap-6 mt-4">
           <div className="w-full aspect-video rounded-xl border border-[#1D1F1E]/10 bg-white overflow-hidden">
             <span className="flex w-full h-full p-3 items-center justify-center">
               <LocationCard />
@@ -86,6 +51,36 @@ export default function Playground() {
             title="Progressive Blur"
             description="Playing around with CSS masks and background blur. I also applied various CSS filters to the video."
             badges={["Next.js", "Tailwind CSS"]}
+          />
+        </section>
+      </FadeInWhenVisible>
+
+      <FadeInWhenVisible delay={0.2}>
+        <section className="flex flex-col gap-6 mt-4">
+          <div className="w-full aspect-video rounded-xl border border-[#1D1F1E]/10 bg-black overflow-hidden">
+            <span className="flex w-full h-full p-3 items-center justify-center">
+              <AudioSlider />
+            </span>
+          </div>
+          <ItemDescription
+            title="Audio Slider"
+            description="Playing with CSS shadows, masks and gradients. Added a small animated SVG there too. Functional slider is a work in progress."
+            badges={["Tailwind CSS"]}
+          />
+        </section>
+      </FadeInWhenVisible>
+
+      <FadeInWhenVisible delay={0.2}>
+        <section className="flex flex-col gap-6 mt-4">
+          <div className="w-full aspect-video rounded-xl border border-[#1D1F1E]/10 bg-black overflow-hidden">
+            <span className="flex w-full h-full items-center justify-center">
+              <NavBarTest />
+            </span>
+          </div>
+          <ItemDescription
+            title="Floating Navbar"
+            description="Simple navbar I created to work on the logic of only having one item active at a time."
+            badges={["Tailwind CSS"]}
           />
         </section>
       </FadeInWhenVisible>
--- app/playground/playground-components/item-description.tsx
+++ app/playground/playground-components/item-description.tsx
@@ -15,13 +15,13 @@ export default function ItemDescription({
 }: ItemDescriptionProps) {
   return (
     <div className="w-full h-fit flex justify-between gap-10">
-      <div className="flex flex-col max-w-60 gap-1">
+      <div className="flex flex-col max-w-60">
         <p className="text-sm tracking-wide text-primary">{title}</p>
         <p className="text-xs text-secondary mt-1 leading-relaxed">
           {description}
         </p>
       </div>
-      <div className="flex flex-wrap justify-end w-full gap-2 h-fit">
+      <div className="flex flex-wrap justify-end w-full max-w-60 gap-2 h-fit">
         {badges?.map(
           (label, index) => label && <Badge key={index} label={label} />
         )}
